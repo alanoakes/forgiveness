82aa1714e78d503dd5f061e084f1987fceca6f33
Parker Oakes <parker.oakes@clkmm.com>
2021-03-20 23:36:49 -0500

    updated WebScraper to iterate over both strongs ids and pages

--- Scripts/WebScraper.sh
+++ Scripts/WebScraper.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
 # Resource: https://www.joyofdata.de/blog/using-linux-shell-web-scraping/
+# Resource (wget): https://unix.stackexchange.com/questions/410493/stop-wget-reusing-existing-connection
+# Data: https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=1&strongs=H5375&ss=1#lexResults
+# Data: https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=1&strongs=H5375#lexResults
 
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=1&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_1.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=2&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_2.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=3&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_3.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=4&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_4.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=5&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_5.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=6&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_6.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=7&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_7.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=8&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_8.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=9&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_9.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=10&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_10.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=11&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_11.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=12&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_12.txt
-#echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=13&strongs=H5375#lexResults" |
-#	wget -O- -i- > ScriptureOut_H5375_13.txt
+StrongsArray=( H3722 H5375 H5545 H5546 H5547 G0859 G0863 G5483 )
 
-for i in {1..13}; do
-	echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=$i&strongs=H5375#lexResults" |
-		wget -O- -i- > ScriptureOut_H5375_$i.txt
+for i in ${StrongsArray[@]}; do
+	for j in {1..20}; do
+
+		echo "https://www.blueletterbible.org/lang/lexicon/lexicon.cfm?page=$j&strongs=$i&ss=1#lexResults" |
+			wget --no-http-keep-alive --no-cache --no-cookies	--header="Accept: text/html" \
+				--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" \
+				-O- -i- |
+			hxnormalize -x |
+			hxselect -i "div.bibleTable" |
+			lynx -stdin -dump -nolist -width=1024 |
+			grep "Unchecked Copy Box " |
+			sed 's/   Unchecked Copy Box //g' >> WebScraperOutput.txt
+
+	done
 done
-#for i in {1..13}; do 
-#	cat ScriptureOut_H5375_$i.txt |
-# 	grep 'class="copyBox hide-for-tablet" data-bible-id=' |
-# 	lynx -stdin -dump -nolist -width=1024 |
-# 	sed 's/Unchecked Copy Box //g' |
-# 	sed '/^$/d' | sed 's/^   //g' > ScriptureOut_H5375_$i.txt
-#
-# echo "... task $i of 13 done"
-#done
